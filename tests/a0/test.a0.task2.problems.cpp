167d32b7ea2cefb261a14fafd5d3e380bb98ab93
taiki <uno.taiki.65r@st.kyoto-u.ac.jp>
2025-05-22 21:03:32 +0900

    a0

--- tests/a0/test.a0.task2.problems.cpp
+++ tests/a0/test.a0.task2.problems.cpp
@@ -11,19 +11,19 @@
 //       compilation errors go away, and move onto the next problem.
 // Hint: there are 3 parts that need to be fixed.
 
-/*
+
 Test test_a0_task2_problems_print("a0.task2.problems.print", []() {
-    string str = "str";
+    std::string str = "str";
     int integer = 0;
     float flt = 0.1f;
 
 	// Most common ways of printing a line of text in Scotty3D are:
-    printf("\n1. printf with format specifiers such as string %s, interger %d, and float %f.\n", str.c_str(), integer, flt)
+    printf("\n1. printf with format specifiers such as string %s, interger %d, and float %f.\n", str.c_str(), integer, flt);
     
-    std::cour << "2. std::cout and std::endl with multiple insertion operators like " 
+    std::cout << "2. std::cout and std::endl with multiple insertion operators like " 
               << str + ", " << integer << ", and " << flt << "." << std::endl;
 });
-*/
+
 
 // A0T2: Problem 2
 // TODO: We want to pass our target 2D vector through a filter called helper, 
@@ -33,7 +33,7 @@ Test test_a0_task2_problems_print("a0.task2.problems.print", []() {
 //       the correct filtered vector.
 
 Test test_a0_task2_problems_numerical("a0.task2.problems.numerical", []() {
-    std::vector<std::vector<int>> target = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::vector<std::vector<int>> target = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // 3 x 3 matrix
     std::vector<int> modifiers = {9, 8, 7, 15, 16, 17, 20, 25, 28};
 
     // This is a lambda function to compare if a third of the modifier is
@@ -41,10 +41,12 @@ Test test_a0_task2_problems_numerical("a0.task2.problems.numerical", []() {
     // Ex) let x = 1, y = 4. y / 3 = 4 / 3 = 1.333 > 1, so x < y / 3. Return true.
 
     int factor = 3;
-    auto helper = [&](int x, int y) { return x < (y / factor); };
+    auto helper = [factor](int x, int y) -> bool { 
+        return float(x) < (float(y) / float(factor)); 
+    };
 
     int j = 0;
-    for (auto& v : target) {
+    for (auto& v : target) {  // auto& (int&) はtargetの中身を置き換え可能
         for (auto& i : v) {
             int y = j >= int(modifiers.size()) ? 0 : modifiers.at(j);
             i = helper(i, y) ? i : 0;
@@ -72,7 +74,10 @@ Test test_a0_task2_problems_vector("a0.task2.problems.vector", []() {
     }
 
     // Use iterator to grab the last element of the vector
-    int last_element = *one_to_ten.end();
+    auto end = one_to_ten.end();
+    --end;
+    int last_element = *end;
+    printf("Last element: %d", last_element);
 
     // The last element is surely a 10... right?
     int expected = 10;
@@ -99,7 +104,7 @@ Test test_a0_task2_problems_boolean("a0.task2.problems.boolean", []() {
         for (size_t j = 0; j < vec2.size(); j++) {
             for (size_t k = 0; k < vec3.size(); k++) {
                 // Check if the numbers at indices i,j,k respectively are the same
-                if ((vec1.at(i) == vec2.at(j)) == vec3.at(k)) count++;
+                if ((vec1.at(i) == vec2.at(j)) && (vec1.at(i) == vec3.at(k))) count++;
             }
         }
     }
